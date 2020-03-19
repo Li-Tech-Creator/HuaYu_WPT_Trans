ce4140b3655bcc221e85ce3950a645003f2051a3
Li-Tech-Creator <2637904651@qq.com>
2020-03-19 15:24:53 +0800

    发射端初始化代码

---
+++ Trans_Basic/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Trans_Basic' 
+ * Target:  'Trans_Basic' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ Trans_Basic/MDK-ARM/Trans_Basic/startup_stm32f407xx.d
@@ -0,0 +1,1 @@
+trans_basic\startup_stm32f407xx.o: startup_stm32f407xx.s
